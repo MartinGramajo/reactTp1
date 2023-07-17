6228eeba09f135e47b0020f2956f05af35537838
¨mago¨ <martingramajo08@gmail.com>
2023-07-17 15:50:23 -0300

    agrego state al componente titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
 const Titulo = ({ nuevoTitulo }) => {
+  // aqui va la logica
+  const [inicial, setInicial] = useState("from changed state");
+
+  // function
+  const cambiarEstado = () => {
+    setInicial("nuevo valor pero con función");
+  };
+
   return (
     <>
       <h1> {nuevoTitulo}</h1>
+      <h2> {inicial} </h2>
+      <Button
+        variant="primary"
+        // onClick={() => setInicial("nuevo valor del estado")}
+        onClick={cambiarEstado}
+      >
+        Cambiar estado
+      </Button>
     </>
   );
 };
